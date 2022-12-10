8654806fff825dee2c4e6fc66e074883d2c94c18
keshew <97288599+keshew@users.noreply.github.com>
2022-12-10 17:06:35 +0500

    test

---
+++ sferaSPM/sferaSPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sferaSPM
+//
+//  Created by Артём Коротков on 10.12.2022.
+//
+
+import UIKit
+import MySPM
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
